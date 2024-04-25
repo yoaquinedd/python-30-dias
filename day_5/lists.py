feb1a5a298198e8f2cef8e47404fb20071b8f49f
Joaquin Poblete <joaquinlolvlad@gmail.com>
2024-04-25 17:14:25 -0400

    seccion de listtas completada

--- day_5/lists.py
+++ day_5/lists.py
@@ -47,3 +47,70 @@ print(does_exist)
 it_companies.sort()
 print(it_companies)
 it_companies.reverse()
+print(it_companies)
+
+print(it_companies[0:3])
+print(it_companies[len(it_companies)-4:len(it_companies)-1])
+
+middle_index = int(len(it_companies)/2)
+
+print(it_companies[middle_index:middle_index+1])
+
+it_companies.pop(0)
+
+print(it_companies)
+
+it_companies.pop(middle_index)
+
+print(it_companies)
+
+it_companies.pop()
+print(it_companies)
+it_companies.clear()
+print(it_companies)
+del it_companies
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+techs = front_end + back_end
+
+full_stack = techs.copy()
+
+print(full_stack)
+
+full_stack.index("Redux")
+print(full_stack.index("Redux"))
+
+# Insertar Python y SQL después de Redux
+full_stack.insert(full_stack.index('Redux') + 1, 'Python')
+full_stack.insert(full_stack.index('Redux') + 2, 'SQL')
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+ages.sort()
+
+print(ages)
+
+last_index = len(ages) - 1
+
+print(ages[0], ages[last_index] )
+
+ages.append(19)
+ages.append(26)
+
+print(ages)
+
+ages.sort()
+
+middle_index = len(ages)//2
+
+ages.sort()
+
+print(ages)
+
+print(middle_index)
+
+print( ages[middle_index]  )
+
+
